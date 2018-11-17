a76ddde2e49f7510715dd364818f8740a18f76cb
Matt Byrnes <mattjbyrnes@gmail.com>
2018-11-16 21:35:05 -0500

    mLab Update DB

--- server.js
+++ server.js
@@ -11,12 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
 // mongoose.connect('mongodb://localhost/todoio', { useNewUrlParser: true });
 mongoose.connect('mongodb://bcmatt:bcpass1@ds163176.mlab.com:63176/heroku_fjn2c96d', { useNewUrlParser: true });
-
-
-// /heroku_6fs4mqgr
 
 require('./sockets/todo-sockets.js')(io);
 require('./routes/html-routes.js')(app);
